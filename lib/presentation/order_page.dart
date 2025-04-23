f16e087037b87dd55d2817db3e1b1940011f21ea
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-23 15:41:10 +0700

    buat struktur awal halaman order

---
+++ lib/presentation/order_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_routing_parsing/navigation/detailpage.dart';
+
+class Orderpage extends StatefulWidget {
+  const Orderpage({super.key});
+
+  @override
+  State<Orderpage> createState() => _OrderpageState();
+}
