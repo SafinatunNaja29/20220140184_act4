0bc821386674ee60afabb01330ec45d28318956a
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-23 15:17:22 +0700

    tambah form register dengan input email dan validasi

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -16,4 +16,23 @@ class _RegisterpageState extends State<Registerpage> {
     super.initState();
   }
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Register'),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
