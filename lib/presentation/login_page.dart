8cf9d5fd267f1f94662e984085b7e9c838e07ff1
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-23 14:55:54 +0700

    tambah tombol login dengan navigasi ke HomePage setelah validasi

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -49,7 +49,19 @@ class _LoginPageState extends State<LoginPage> {
                   }
                   return null;
                 },
-              )
+              ),
+              ElevatedButton(
+                onPressed: () { 
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  }
+                },
+                child: Text('Login'),
+              ),
+              
             ],
           )
         ),
