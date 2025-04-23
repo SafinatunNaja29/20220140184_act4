82858c0a428ec1940abac421dd2434a99aaf7e03
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-23 14:34:44 +0700

    initial commit

---
+++ lib/presentation/login_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
+import 'package:play_navigation/presentation/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            MainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+            ],
+          )
+        ),
+      ),
+    )
+  }
+}
